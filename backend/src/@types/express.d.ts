68ce506a736ac79059bf5b2ecee6b005df39b480
gustavoml12 <gustavoml12@gmail.com>
2025-02-20 10:32:23 -0300

    deploy 7

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -1,15 +1,41 @@
-declare namespace Express {
-  export interface Request {
-    user: { id: string; profile: string; isSuper: boolean, companyId: number };
-    companyId: number | undefined,
-    tokenData: {
-      id: string;
-      username: string;
-      profile: string;
-      super: boolean;
-      companyId: number;
-      iat: number;
-      exp: number;
-    } | undefined
+import 'express';
+import { Multer } from 'multer';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: { 
+        id: string; 
+        profile: string; 
+        isSuper: boolean;
+        companyId: number;
+      };
+      companyId: number | undefined;
+      tokenData: {
+        id: string;
+        username: string;
+        profile: string;
+        super: boolean;
+        companyId: number;
+        iat: number;
+        exp: number;
+      } | undefined;
+      file?: Express.Multer.File;
+      files?: Express.Multer.File[];
+    }
+
+    namespace Multer {
+      interface File {
+        fieldname: string;
+        originalname: string;
+        encoding: string;
+        mimetype: string;
+        size: number;
+        destination: string;
+        filename: string;
+        path: string;
+        buffer: Buffer;
+      }
+    }
   }
 }
