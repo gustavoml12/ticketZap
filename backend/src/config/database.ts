b852eaaf53c0531ed7d2899a2ca8b3234b74a3f1
gustavoml12 <gustavoml12@gmail.com>
2025-02-19 21:14:35 -0300

    ajustando devops

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,8 +2,8 @@ import "../bootstrap";
 
 module.exports = {
   define: {
-    charset: "utf8mb4",
-    collate: "utf8mb4_bin"
+    charset: "utf8",
+    collate: "utf8_general_ci"
   },
   pool: {
     max: process.env.DB_MAX_CONNECTIONS || 60,
@@ -11,13 +11,17 @@ module.exports = {
     acquire: process.env.DB_ACQUIRE || 30000,
     idle: process.env.DB_IDLE || 10000
   },
-  dialect: process.env.DB_DIALECT || "mysql",
+  url: process.env.DATABASE_URL,
+  dialect: "postgres",
   timezone: process.env.DB_TIMEZONE || "-03:00",
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   logging: process.env.DB_DEBUG && console.log,
-  seederStorage: "sequelize"
+  seederStorage: "sequelize",
+  dialectOptions: {
+    ssl: false
+  }
 };
