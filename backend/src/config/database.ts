25bfd4b2a0dee3e74fe31ec1982069543a831712
gustavoml12 <gustavoml12@gmail.com>
2025-02-25 07:45:54 -0300

    deploy 90

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -6,20 +6,15 @@ module.exports = {
     collate: "utf8_general_ci"
   },
   pool: {
-    max: process.env.DB_MAX_CONNECTIONS || 60,
-    min: process.env.DB_MIN_CONNECTIONS || 5,
-    acquire: process.env.DB_ACQUIRE || 30000,
-    idle: process.env.DB_IDLE || 10000
+    max: 60,
+    min: 5,
+    acquire: 30000,
+    idle: 10000
   },
   url: process.env.DATABASE_URL,
   dialect: "postgres",
-  timezone: process.env.DB_TIMEZONE || "-03:00",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  logging: process.env.DB_DEBUG && console.log,
+  timezone: "-03:00",
+  logging: false,
   seederStorage: "sequelize",
   dialectOptions: {
     ssl: false
